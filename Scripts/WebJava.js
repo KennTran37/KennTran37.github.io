ae39e0e055ca52ff2edfd6d058b1883c513b45ac
Kenny Tran <kenn_tran_37@hotmail.com>
2021-07-11 17:11:38 +1000

    Website Redesign v2
    - Redesigned website after taking inspiration from https://alkamzk.com/'s simplistic design for the landing page

--- Scripts/WebJava.js
+++ Scripts/WebJava.js
@@ -2,57 +2,34 @@ window.onload = function(){
     for(var i = 0; i < mediaIcons.length; i++)
         AssignIconEvent(mediaIcons[i]);
     
-    //Loading Project Info
-    LoadProjectListJSON().then(async ()=>{  //Loading Project Cards
-        for(var i = 0; i < projectList.length; i++){
-            var _card = document.createElement("div");
-            _card.setAttribute("class", "ProjectCard");
-            if(window.outerWidth <= 450)
-                _card.style.width = (projectsSegment.clientWidth / 2) - 20 + 'px';
-            else
-                _card.style.width = (projectsSegment.clientWidth / 3) - 17.5 + 'px';
-            AddCardEvent(_card, projectList[i].downloadUrl);
-            
-            var _icon = document.createElement("IMG");
-            _icon.src = projectList[i].projectIcon;
-            
-            var _name = document.createElement("h3");
-            _name.innerText = projectList[i].projectName;
-            
-            var _role = document.createElement("p");
-            _role.innerText = projectList[i].projectRole;
-            _role.style.fontWeight = 600;
-            
-            var _description = document.createElement("p");
-            _description.innerText = projectList[i].description;
-            _description.style.fontSize = 15 + 'px';
-            
-            var _lineBreak = document.createElement("IMG");
-            _lineBreak.style.width = 150 + 'px';
-            _lineBreak.style.height = 24 + 'px';
-            _lineBreak.src = "Images/Line_break.png";
-            
-            _card.append(_icon);
-            _card.append(document.createElement("br"));
-            _card.append(_name);
-            _card.append(_lineBreak);
-            _card.append(_role);
-            _card.append(_lineBreak);
-            _card.append(_description);
-            projectsSegment.append(_card);
-        }
-    }).then(()=>{   //Loading Project Screenshots
-        $.get("/screenshots.text", function(data){
-            const screenshots = data.split(/\r\n|\n/);
-            for(var i = 0; i < screenshots.length; i++){
-                var img = document.createElement("IMG");
-                img.src = "/Images/Projects/Screenshots/" + screenshots[i];
-                mediaSegment.append(img);
-            }
-        }, 'text');
+    LoadProjectListJSON().then(async ()=>{
+       for(var i = 0; i < projectList.length; i++){
+           var _card = document.createElement("div");
+           _card.setAttribute("class", "ProjectCard");
+           
+           AddCardEvent(_card, projectList[i].downloadUrl);
+           
+           //Create project icon
+           var _icon = document.createElement("IMG");
+           _icon.src = projectList[i].projectIcon;
+           
+           //Create project name label
+           var _name = document.createElement("h3");
+           _name.innerText = projectList[i].projectName;           
+           
+           //Create role label
+           var _role = document.createElement("p");
+           let _roleNames =  projectList[i].projectRole.replaceAll('|', "<span>&#8226;</span>");
+           _role.innerHTML = _roleNames;
+           
+           _card.append(_icon);
+           _card.append(_name);
+           _card.append(_role);
+           projectsSegment.append(_card);
+       } 
     });
 }
-
+    
 function AddCardEvent(_card ,_url){
     if(_url == "") return;
     _card.addEventListener("mouseover", function(){
@@ -63,38 +40,6 @@ function AddCardEvent(_card ,_url){
     });
 }
 
-function AssignIconEvent(iconInfo){
-    iconInfo[0].addEventListener("mouseover", function(){
-       iconInfo[0].src = iconInfo[1];
-    });
-    iconInfo[0].addEventListener("mouseout", function(){
-       iconInfo[0].src = iconInfo[2];
-    });
-}
-
-var mediaIcons = [
-    [
-        document.getElementById("emailIcon"),
-        "Images/SocialIcons/Logo_Email_Dark.png",
-        "Images/SocialIcons/Logo_Email.png"
-    ],
-    [
-        document.getElementById("twitterIcon"),
-        "Images/SocialIcons/Logo_Twitter_Dark.png",
-        "Images/SocialIcons/Logo_Twitter.png"
-    ],
-    [
-        document.getElementById("youtubeIcon"),
-        "Images/SocialIcons/Logo_Youtube_Dark.png",
-        "Images/SocialIcons/Logo_Youtube.png"
-    ],
-    [
-        document.getElementById("linkedInIcon"),
-        "Images/SocialIcons/Logo_LinkedIn_Dark.png",
-        "Images/SocialIcons/Logo_LinkedIn.png"
-    ],
-];
-
 var mediaSegment = document.getElementById("media");
 var projectsSegment = document.getElementById("projects");
 var projectList;
@@ -102,7 +47,7 @@ async function LoadProjectListJSON(){
     await fetch("/Scripts/ProjectList.json")
         .then(response =>{
             if(!response.ok){
-                throw new Error('Network responce was not ok');
+                throw new Error('Network response failed!');
             }
             return response.json();
         })
@@ -110,3 +55,35 @@ async function LoadProjectListJSON(){
             projectList = data;
         });
 }
+
+function AssignIconEvent(iconInfo){
+    iconInfo[0].addEventListener("mouseover", function(){
+       iconInfo[0].src = iconInfo[2];
+    });
+    iconInfo[0].addEventListener("mouseout", function(){
+       iconInfo[0].src = iconInfo[1];
+    });
+}
+
+var mediaIcons = [
+    [
+        document.getElementById("githubIcon"),
+        "Images/SocialIcons/Logo_Github.png",
+        "Images/SocialIcons/Logo_Github_Highlight.png",
+    ],
+    [
+        document.getElementById("linkedinIcon"),
+        "Images/SocialIcons/Logo_LinkedIn.png",
+        "Images/SocialIcons/Logo_LinkedIn_Highlight.png",
+    ],
+    [
+        document.getElementById("twitterIcon"),
+        "Images/SocialIcons/Logo_Twitter.png",
+        "Images/SocialIcons/Logo_Twitter_Highlight.png",
+    ],
+    [
+        document.getElementById("youtubeIcon"),
+        "Images/SocialIcons/Logo_Youtube.png",
+        "Images/SocialIcons/Logo_Youtube_Highlight.png",
+    ]
+];
