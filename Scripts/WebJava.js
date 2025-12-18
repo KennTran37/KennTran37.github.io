ecfe75e84820e80806c2baabd6e20aa14d99ff01
Kenny Tran <61035350+KennTran37@users.noreply.github.com>
2025-12-18 20:39:08 +1000

    Removed technical skills tab

--- Scripts/WebJava.js
+++ Scripts/WebJava.js
@@ -31,25 +31,25 @@ function changeTab(pageName) {
         tab.classList.add("active");
     }
 
-    tabContentContainer = document.getElementById("tabContentContainer");
-    if (tabContentContainer) {
-        const isWideScreen = window.innerWidth > 600;
-        if (isWideScreen) {
-            switch (pageName) {
-                case 'resume':
-                    tabContentContainer.style.borderRadius = "0 4px 4px 4px";
-                    break;
-                case 'portfolio':
-                    tabContentContainer.style.borderRadius = "4px";
-                    break;
-                case 'skills':
-                    tabContentContainer.style.borderRadius = "4px 0 4px 4px";
-                    break;
-            }
-        } else {
-            tabContentContainer.style.borderRadius = "4px";
-        }
-    }
+    // tabContentContainer = document.getElementById("tabContentContainer");
+    // if (tabContentContainer) {
+    //     const isWideScreen = window.innerWidth > 600;
+    //     if (isWideScreen) {
+    //         switch (pageName) {
+    //             case 'resume':
+    //                 tabContentContainer.style.borderRadius = "0 4px 4px 4px";
+    //                 break;
+    //             case 'portfolio':
+    //                 tabContentContainer.style.borderRadius = "4px";
+    //                 break;
+    //             case 'skills':
+    //                 tabContentContainer.style.borderRadius = "4px 0 4px 4px";
+    //                 break;
+    //         }
+    //     } else {
+    //         tabContentContainer.style.borderRadius = "4px";
+    //     }
+    // }
 }
 
 window.onload = function () {
